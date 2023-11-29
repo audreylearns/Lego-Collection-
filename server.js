faeee3327b5c6009694389d88c9a9310e5c6a4a7
audreylearns <audreyduzon.cda@GMAIL.COM>
2023-11-29 18:22:03 -0500

    port to 8080 instead

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ const clientSessions = require('client-sessions');
 
 const express = require('express');
 const app = express();
-const HTTP_PORT = process.env.PORT || 3000;
+const HTTP_PORT = process.env.PORT || 8080;
 
 //app.listen(HTTP_PORT, () => console.log('Connection established at PORT '  + HTTP_PORT));
 app.set('view engine', 'ejs'); //new addition A4
@@ -60,7 +60,7 @@ legoData.Initialize()
 app.use(
     clientSessions({
       cookieName: 'session', // this is the object name that will be added to 'req'
-      secret: 'thisisAudreysassignmentforWeb322FinalTHISWASQUITEHARD', // this should be a long un-guessable string.
+      secret: 'o6LjQ5EVNC28ZgK64hDELM18ScpFQr', // this should be a long un-guessable string.
       duration: 2 * 60 * 1000, // duration of the session in milliseconds (2 minutes)
       activeDuration: 1000 * 60, // the session will be extended by this many ms each request (1 minute)
     })

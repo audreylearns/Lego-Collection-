b6b409b549883e3d4a828e8e0ce4175c5b1350f7
audreylearns <audreyduzon.cda@GMAIL.COM>
2023-11-29 17:55:29 -0500

    secret changed

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ legoData.Initialize()
 app.use(
     clientSessions({
       cookieName: 'session', // this is the object name that will be added to 'req'
-      secret: 'o6LjQ5EVNC28ZgK64hDELM18ScpFQr', // this should be a long un-guessable string.
+      secret: 'thisisAudreysassignmentforWeb322FinalTHISWASQUITEHARD', // this should be a long un-guessable string.
       duration: 2 * 60 * 1000, // duration of the session in milliseconds (2 minutes)
       activeDuration: 1000 * 60, // the session will be extended by this many ms each request (1 minute)
     })
